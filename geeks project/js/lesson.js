3fa9457a385a8daa78a70bdb726ec2d5fff490ca
永华 <ryskulovasaykal10@gmail.com>
2024-10-20 21:13:57 +0600

    Add files via upload

--- geeks project/js/lesson.js
+++ geeks project/js/lesson.js
@@ -80,53 +80,88 @@ xhr.send()
 
 // conveter
 
-const usdInput = document.querySelector('#usd')
-const somInput = document.querySelector('#som')
-
-const converter = (element, targetElement) => {
-    element.oninput = () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', '../data/converter.json')
-        request.setRequestHeader('Content-type', 'application/json')
-        request.send()
-
-        request.onload = () => {
-            const data = JSON.parse(request.response)
-            if (element.id === 'som') {
-                targetElement.value = (element.value / data.usd).toFixed(2)
-            }
-            if (element.id === 'usd') {
-                targetElement.value = (element.value * data.usd).toFixed(2)
-            }
-            if (element.value === '') {
-                targetElement.value = ''
-            }
-        }
-    }
-}
-converter(usdInput, somInput)
-converter(somInput, usdInput)
-
-// somInput.oninput = () => {
-//     const request = new XMLHttpRequest();
-//     request.open('GET', '../data/converter.json')
-//     request.setRequestHeader('Content-type', 'application/json')
-//     request.send()
 //
-//     request.onload = () => {
-//         const data = JSON.parse(request.response)
-//         usdInput.value = (somInput.value / data.usd).toFixed(2)
+// const eurInput = document.querySelector('#eur')
+// const usdInput = document.querySelector('#usd')
+// const somInput = document.querySelector('#som')
+//
+//
+// const converter = (element, targetElement) => {
+//     element.oninput = () => {
+//         const request = new XMLHttpRequest();
+//         request.open('GET', '../data/converter.json')
+//         request.setRequestHeader('Content-type', 'application/json')
+//         request.send()
+//
+//         request.onload = () => {
+//             const data = JSON.parse(request.response)
+//             if (element.id === 'som') {
+//                 targetElement.value = (element.value / data.usd).toFixed(2)
+//             }
+//             if (element.id === 'usd') {
+//                 targetElement.value = (element.value * data.usd).toFixed(2)
+//             }
+//             if (element.id === 'eur') {
+//                 targetElement.value = (element.value * data.eur).toFixed(2)
+//             }
+//             if (element.value === '') {
+//                 targetElement.value = ''
+//             }
+//         }
 //     }
 // }
-//
-// usdInput.oninput = () => {
-//     const request = new XMLHttpRequest();
-//     request.open('GET', '.../data/converter.json')
-//     request.setRequestHeader('Content-type', 'application/json')
-//     request.send()
-//
-//     request.onload = () => {
-//         const data = JSON.parse(request.response)
-//         somInput.value = (usdInput.value * data.usd).toFixed(2)
-//     }
-// }
+// converter(usdInput, somInput)
+// converter(somInput, usdInput)
+// converter(eurInput, usdInput)
+// converter(usdInput, eurInput)
+// converter(somInput, eurInput)
+// converter(eurInput, somInput)
+
+
+
+// CARD
+const card = document.querySelector('.card');
+const prevButton = document.querySelector('#btn-prev');
+const nextButton = document.querySelector('#btn-next');
+
+let cardId = 1;
+
+const fetchCard = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Ошибка загрузки карточки');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const { id, title, completed } = data;
+            card.innerHTML = `
+                <p>${title}</p>
+                <p>${completed}</p>
+                <span>${id}</span>
+            `;
+        })
+};
+
+
+fetchCard(cardId);
+
+nextButton.onclick = () => {
+    cardId = cardId < 200 ? cardId + 1 : 1;
+    fetchCard(cardId);
+};
+
+prevButton.onclick = () => {
+    cardId = cardId > 1 ? cardId - 1 : 200;
+    fetchCard(cardId);
+};
+
+// fetch
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
